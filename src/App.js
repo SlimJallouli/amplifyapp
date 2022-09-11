ce425684527d82874c233be0920e221559a11666
SlimJallouli <slim.jallouli@st.com>
2022-09-11 12:05:13 -0700

    add amplify

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Hello />)
+
+function Hello(props) {
+  return <h1>Hello World!</h1>;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
+      <h1>Hello World!</h1>
     </div>
   );
 }
